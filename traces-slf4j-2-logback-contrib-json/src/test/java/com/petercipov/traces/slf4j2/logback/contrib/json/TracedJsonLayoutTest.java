ab8ae844eacdcae5e1df2c2e8484eee5f426e1fb
Peter Cipov <petercipov@gmail.com>
2022-10-02 09:59:54 +0200

    remove obsolete modules, added tests

--- traces-slf4j-2-logback-contrib-json/src/test/java/com/petercipov/traces/slf4j2/logback/contrib/json/TracedJsonLayoutTest.java
+++ traces-slf4j-2-logback-contrib-json/src/test/java/com/petercipov/traces/slf4j2/logback/contrib/json/TracedJsonLayoutTest.java
@@ -1,10 +1,13 @@
-package com.petercipov.traces.slf4j2.logback;
+package com.petercipov.traces.slf4j2.logback.contrib.json;
 
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.ConsoleAppender;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.petercipov.traces.slf4j2.testutils.ConsoleBuffer;
 import com.petercipov.traces.slf4j2.testutils.UserTrace;
 import org.junit.jupiter.api.Assertions;
@@ -13,9 +16,11 @@ import org.junit.jupiter.api.extension.RegisterExtension;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-class MarkerTest {
+import static org.junit.jupiter.api.Assertions.*;
 
-    public static final Logger LOGGER = LoggerFactory.getLogger(MarkerTest.class);
+class TracedJsonLayoutTest {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(TracedJsonLayoutTest.class);
 
     @RegisterExtension
     static ConsoleBuffer buffer = new ConsoleBuffer();
@@ -24,15 +29,14 @@ class MarkerTest {
     public void markerIsProperlyTransformedIntoOutput() {
 
         LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
-        PatternLayoutEncoder ple = new PatternLayoutEncoder();
 
-        ple.setPattern("[%marker] - %msg");
-        ple.setContext(lc);
-        ple.start();
+        TracedJsonLayout tracedJsonLayout = new TracedJsonLayout();
+        tracedJsonLayout.setContext(lc);
+        tracedJsonLayout.start();
 
         ConsoleAppender<ILoggingEvent> appender = new ConsoleAppender<>();
         appender.setContext(lc);
-        appender.setEncoder(ple);
+        appender.setLayout(tracedJsonLayout);
         appender.start();
 
         ch.qos.logback.classic.Logger ll = (ch.qos.logback.classic.Logger)LOGGER;
@@ -47,10 +51,20 @@ class MarkerTest {
         LOGGER.info(trace, "XYZ");
 
 
-        Assertions.assertEquals("[accountId: Ac32463a2-3466-463a-b02c-bb5fe1561b80, " +
-                "userId: U30361475-6ea0-4904-a767-fa0c54e486bb, " +
-                "invocationId: I0f6bf39e-639c-4a53-a818-7ff9fb8482af] - XYZ", buffer.getConsoleOutput());
+        JsonObject json = new Gson().fromJson(buffer.getConsoleOutput().trim(), JsonObject.class);
+
+        json.remove("timestamp");
+        json.remove("thread");
+        json.remove("logger");
+        json.remove("context");
+
+
+        Assertions.assertEquals(
+                "{\"level\":\"INFO\",\"message\":\"XYZ\",\"accountId\":\"Ac32463a2-3466-463a-b02c-bb5fe1561b80\"," +
+                "\"userId\":\"U30361475-6ea0-4904-a767-fa0c54e486bb\",\"invocationId\":\"I0f6bf39e-639c-4a53-a818-7ff9fb8482af\"}",
+                json.toString());
 
 
     }
+
 }
